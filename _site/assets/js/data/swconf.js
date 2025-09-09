1e4a49f1ccf59b08f41690af91fa03a66c1f541c
chenchengliang <lcckkkhaha@163.com>
2025-09-10 00:36:42 +0200

    updates github pages deploy workflow

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1757456488',resources: [
+    cacheName: 'chirpy-1757457388',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

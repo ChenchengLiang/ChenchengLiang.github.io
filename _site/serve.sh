6edef7a019a6b37c1aac1bed31a3f51a6ba64278
chenchengliang <lcckkkhaha@163.com>
2025-09-09 21:34:06 +0200

    update

---
+++ _site/serve.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+docker run --rm -p 4000:4000 -p 35729:35729 \
+  -v "$PWD":/site -w /site ruby:3.2-bookworm bash -lc \
+  "apt-get update && apt-get install -y build-essential >/dev/null && \
+   gem install bundler >/dev/null && bundle config set path vendor/bundle && \
+   bundle install && bundle exec jekyll serve --livereload --incremental --host 0.0.0.0"

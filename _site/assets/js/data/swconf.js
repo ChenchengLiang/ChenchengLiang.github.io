778dc4fc158fc1fdfb9a527c33945d61ef2dfaa3
chenchengliang <lcckkkhaha@163.com>
2025-09-18 19:30:27 +0200

    update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758147145',resources: [
+    cacheName: 'chirpy-1758216584',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       

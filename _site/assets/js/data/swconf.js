aef9d8618d684db8a3de8c12c84eba03208cc594
chenchengliang <lcckkkhaha@163.com>
2025-09-18 19:34:25 +0200

    Update .gitignore, feed.xml, sitemap.xml, and swconf.js with new cache name and last modified timestamps

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758216672',resources: [
+    cacheName: 'chirpy-1758216858',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
